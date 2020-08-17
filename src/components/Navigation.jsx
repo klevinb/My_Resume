046a7b2249f0b1027ba37ead77ee23d55f2352cd
Klevin Bazaiti <klevin.bazaiti98@gmail.com>
2020-08-17 15:16:00 +0200

    youtube videos search done!

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,32 @@ import { Image } from "react-bootstrap";
 function Navigation({ navSelected, setSelectedNav }) {
   return (
     <>
-      <Image className='logo_img' src='/assets/logo_night.png' alt='logo' />
+      <div className='nav'>
+        <Image className='logo_img' src='/assets/logo_night.png' alt='logo' />
+        <ul className='socials'>
+          <a
+            href='https://github.com/klevinb'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <li>GitHub</li>
+          </a>
+          <a
+            href='https://www.linkedin.com/in/klevinbazaiti/'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <li>LinkedIn</li>
+          </a>
+          <a
+            href='https://www.instagram.com/klevinb/'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <li>Instagram</li>
+          </a>
+        </ul>
+      </div>
       <ul className='nav-bar'>
         <li
           className={navSelected === "Education" ? "selected" : ""}
@@ -28,29 +53,6 @@ function Navigation({ navSelected, setSelectedNav }) {
         >
           Contacts
         </li>
-      </ul>
-      <ul className='socials'>
-        <a
-          href='https://github.com/klevinb'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <li>GitHub</li>
-        </a>
-        <a
-          href='https://www.linkedin.com/in/klevinbazaiti/'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <li>LinkedIn</li>
-        </a>
-        <a
-          href='https://www.instagram.com/klevinb/'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <li>Instagram</li>
-        </a>
       </ul>
     </>
   );
