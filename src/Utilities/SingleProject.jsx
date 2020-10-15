d05633f35db871991398036736fb11039561a4f8
klevinb <klevin.bazaiti98@gmail.com>
2020-10-16 00:54:25 +0200

    new way of presenting projects

--- src/Utilities/SingleProject.jsx
+++ src/Utilities/SingleProject.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Row, Col, Image, Button } from 'react-bootstrap';
 import { AiOutlineGithub } from 'react-icons/ai';
+import { GrHeroku } from 'react-icons/gr';
 import styles from './SingleProject.module.scss';
 
 function SingleProject(props) {
@@ -12,15 +13,40 @@ function SingleProject(props) {
         <h3>{props.title}</h3>
         <p>{props.description}</p>
 
-        <p>{props.tools}</p>
-        <a href={props.repoLink} rel='noopener noreferrer' target='_blank'>
-          <Button>
-            <AiOutlineGithub /> Repo Link
-          </Button>
-        </a>
+        <p>{props.toolsFE}</p>
+        <p>{props.apis}</p>
+        <p>{props.toolsBE}</p>
+        {props.liveLink && (
+          <a href={props.liveLink} rel='noopener noreferrer' target='_blank'>
+            <Button>
+              <GrHeroku /> Live Demo
+            </Button>
+          </a>
+        )}
+        <div className='d-flex jusify-flex-center'>
+          {props.feLink && (
+            <a href={props.feLink} rel='noopener noreferrer' target='_blank'>
+              <Button>
+                <AiOutlineGithub /> FE Repo Link
+              </Button>
+            </a>
+          )}
+          {props.beLink && (
+            <a href={props.beLink} rel='noopener noreferrer' target='_blank'>
+              <Button>
+                <AiOutlineGithub /> BE Repo Link
+              </Button>
+            </a>
+          )}
+        </div>
       </Col>
-      <Col sm={12} md={6}>
-        <Image fluid src='/assets/SpotifyApp/spotify.jpg' />
+      <Col sm={12} md={6} className={styles.Logo}>
+        <div className={styles.Container}>
+          <Image fluid src={props.logo} />
+          <div className={styles.Middle}>
+            <button className={styles.Button}>View Photos</button>
+          </div>
+        </div>
       </Col>
     </Row>
   );
--- src/components/Pages/Projects/index.jsx
+++ src/components/Pages/Projects/index.jsx
@@ -11,29 +11,112 @@ export default function Projects() {
         <div className={styles.Project}>
           <div className={styles.SingleProject}>
             <SingleProject
-              title='Spotify App Mockup'
-              description='I created a mockup for the Spotify website using React, Redux,
-            Bootstrap and later I will implement the login page, and then I will
-use MongoDB to store the data.'
-              repoLink='https://github.com/klevinb/CV-Project/tree/master/spotify_alone'
-              tools='API : Deezer - RapidAPI.'
+              title='YOLO - You Only Live Once'
+              description='This was my solo capstone, where I build an app which is a 
+              mix between EventBrite but mostly party events and a social app.
+               This app was created for people that want to party everyday and 
+              they are open to create new friendships'
+              feLink='https://github.com/klevinb/Solo_Capstone_FE'
+              liveLink='https://yolowebsitefe.herokuapp.com'
+              beLink='https://github.com/klevinb/Solo_Capstone_BE'
+              toolsFE='Front End : React, Bootstrap, MaterialUI'
+              toolsBE='Back End : Node, Express, MongoDb, JWT, Socketio, Sendgrid, PdfMake, Facebook OAuth'
+              logo='/assets/YoloApp/yolo.png'
+              photos={[
+                {
+                  title: 'Home Page',
+                  desktopView: '/assets/SpotifyApp/Home_page.jpg',
+                  mobileView: '/assets/SpotifyApp/Home_page_mobile.png',
+                },
+                {
+                  title: 'Album Page',
+                  desktopView: '/assets/SpotifyApp/Album_page.jpg',
+                  mobileView: '/assets/SpotifyApp/Album_page_mobile.png',
+                },
+                {
+                  title: 'Favorite Page',
+                  desktopView: '/assets/SpotifyApp/Favorite_page.jpg',
+                  mobileView: '/assets/SpotifyApp/Favorite_page_mobile.png',
+                },
+                {
+                  title: 'Search Page',
+                  desktopView: '/assets/SpotifyApp/Search_page.jpg',
+                  mobileView: '/assets/SpotifyApp/Search_page_mobile.png',
+                },
+              ]}
             />
           </div>
         </div>
         <div className={styles.Project1}>
           <div className={styles.SingleProject}>
             <SingleProject
-              title='LinkedIn Mockup'
-              description='This was a team project, we used React, Node, Express, MongoDB. 
-          For the Backend we created endpoints for Posts, Photos, Experiences (CSV), CV (PDF), log in - sign up pages.
-          We used JSON Web Token and socket.io for the chatrooms.'
-              repoLink='https://github.com/klevinb/LinkedinBackend_TBW_FE'
-              tools=''
+              title='Spotify App Mockup'
+              description='I created a mockup for the Spotify website using React, Redux,
+            Bootstrap and later I will implement the login page, and then I will
+use MongoDB to store the data.'
+              feLink='https://github.com/klevinb/CV-Project/tree/master/spotify_alone'
+              liveLink=''
+              beLink='https://github.com/klevinb/Spotify_Backend'
+              apis='API : Deezer - RapidAPI.'
+              toolsFE='Front End : React, Bootstrap'
+              toolsBE='Back End : Node, Express, MongoDb, JWT'
+              logo='/assets/SpotifyApp/spotify.jpg'
+              photos={[
+                {
+                  title: 'Home Page',
+                  desktopView: '/assets/SpotifyApp/Home_page.jpg',
+                  mobileView: '/assets/SpotifyApp/Home_page_mobile.png',
+                },
+                {
+                  title: 'Album Page',
+                  desktopView: '/assets/SpotifyApp/Album_page.jpg',
+                  mobileView: '/assets/SpotifyApp/Album_page_mobile.png',
+                },
+                {
+                  title: 'Favorite Page',
+                  desktopView: '/assets/SpotifyApp/Favorite_page.jpg',
+                  mobileView: '/assets/SpotifyApp/Favorite_page_mobile.png',
+                },
+                {
+                  title: 'Search Page',
+                  desktopView: '/assets/SpotifyApp/Search_page.jpg',
+                  mobileView: '/assets/SpotifyApp/Search_page_mobile.png',
+                },
+              ]}
             />
           </div>
         </div>
         <div className={styles.Project2}>
-          <div className={styles.SingleProject}></div>
+          <div className={styles.SingleProject}>
+            <SingleProject
+              title='LinkedIn Mockup'
+              description='This was a team project, we used React, Node, Express, MongoDB. 
+          For the Backend we created endpoints for Posts, Photos, Experiences (CSV), CV (PDF), log in - sign up pages.
+          We used JSON Web Token and socket.io for the chatrooms.'
+              feLink='https://github.com/klevinb/LinkedinBackend_TBW_FE'
+              liveLink=''
+              beLink='https://github.com/klevinb/LinkedinBackend_TBW_AUTH'
+              tools=''
+              logo='/assets/LinkedInApp/linkedin.png'
+              photos={[
+                {
+                  title: 'User Page',
+                  desktopView: '/assets/LinkedInApp/UsersPage.png',
+                  mobileView: '',
+                },
+                {
+                  title: 'LinkedIn User',
+                  desktopView: '/assets/LinkedInApp/OtherUserPage.png',
+                  mobileView: '',
+                },
+                {
+                  title: 'Feeds Page',
+                  desktopView: '/assets/LinkedInApp/FeedsPage.png',
+                  mobileView: '',
+                },
+              ]}
+            />
+          </div>
         </div>
       </div>
     </>
