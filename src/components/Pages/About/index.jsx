57dc66920995be7759623335c58974935e5c23cc
klevinb <klevin.bazaiti98@gmail.com>
2020-11-03 19:48:33 +0100

    new photo url fixed

--- src/components/Pages/About/index.jsx
+++ src/components/Pages/About/index.jsx
@@ -18,10 +18,7 @@ export default function About() {
               rel='noopener noreferrer'
               target='_blank'
             >
-              <Image
-                fluid
-                src='https://instagram.fosl4-1.fna.fbcdn.net/v/t51.2885-15/e35/40634221_454554298399748_272423315954466816_n.jpg?_nc_ht=instagram.fosl4-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=TA9P3GYq8GQAX-sEjzM&_nc_tp=18&oh=3672ae2e966899a727e10e9270eedd90&oe=5FA040EB'
-              />
+              <Image fluid src='/assets/about_me.jpg' />
             </a>
           </Col>
           <Col sm={12} md={6}>
